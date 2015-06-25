da031f1b0ce910eab508eadf095ec834f322e954
sterling <sterlingsrq@gmail.com>
2015-06-24 20:55:15 -0400

    Add functions chooseAsset, getTimeInHours, buildBackgroundId with tests.

--- src/background.es6.js
+++ src/background.es6.js
@@ -1,8 +1,97 @@
-class Foo {
+/**
+ *
+ */
+class Background {
 
-    doSomething() {
-        return 'Do Something';
+
+    constructor(args) {
+        if (!args) { throw new Error("An options object is required"); }
+        if (!args.dayAsset || !args.nightAsset) { throw new Error("The options object must include a 'dayAsset' and 'nightAsset'."); }
+
+        this.dayAsset            = args.dayAsset;
+        this.nightAsset          = args.nightAsset;
+        this.backgroundElementId = this.buildBackgroundId(this.backgroundElementId);
+        this.previousAsset       = null;
+        this.backgroundCss       = ""
     }
+
+
+    /**
+     * @param {string} id - id of the element whos background will be changed.
+     */
+    buildBackgroundId(id) {
+        if (!id) { 
+            return "#background"; 
+        }
+
+        if (id.charAt(0) == "#") {
+            return id;
+        } else {
+            return ("#" + id);
+        }
+    }
+
+
+    /**
+     * 
+     */
+    start() {
+        setInterval(()=>{
+            var asset = this.chooseAsset(this.getTimeInHours());
+
+            if (asset != this.previousAsset) {
+                this.changeBackground(asset);
+                this.previousAsset = asset;
+            }
+        }, 200);
+    }
+
+
+    /**
+     * @return - element object of the element whos background was updated
+     */
+    changeBackground(asset) {
+        if (!asset) { throw new Error("An asset is required."); }
+        var element = document.getElementById(this.backgroundElementId);
+
+        element.style.background = "url(${asset})";
+
+        return element;
+    }
+
+
+    /**
+     * @return - current hour of the day (24 hour format)
+     */
+    getTimeInHours() {
+        return new Date().getHours();
+    }
+
+
+    /**
+     * Return what asset to use based on the given hour.
+     *
+     * @param {number} hour - current hour of the day (24 hour format)
+     */
+    chooseAsset(hour) {
+        switch (true) {
+            // Day: 6am - 7pm
+            case ((hour >= 6) && (hour <= 19)):
+                return this.dayAsset;
+            break;
+            // Night: 8pm - 5am
+            case (((hour >= 20) && (hour <= 24)) || ((hour <= 5) && (hour >= 0))):
+                return this.nightAsset;
+            break;
+            default:
+                console.log("Error deciding what asset to use. Defaulting to dayAsset.");
+                return this.dayAsset;
+            break;
+        }
+    }
+
+
+
 };
 
-export default Foo;
+export default Background;
--- test/background.es6.spec.js
+++ test/background.es6.spec.js
@@ -1,16 +1,102 @@
-import Foo from '../src/background.es6.js';
+import Background from '../src/background.es6.js';
 
-console.log("Running test");
 
-describe('ES6 Foo', function () {
+describe('Background Class', ()=>{
+    let background = null,
+        myEl       = null,
+        dayAsset   = "../day.png",
+        nightAsset = "../night.png";
 
-    let foo;
+
+    describe('constructor', ()=>{
+        it('requires an options object', ()=>{
+            expect(()=>{ new Background(); }).toThrowError();
+        });
+    });
+
 
     beforeEach(()=>{
-        foo = new Foo();
+        background = new Background({
+            dayAsset:   dayAsset,
+            nightAsset: nightAsset
+        });
+
+        document.body.insertAdjacentHTML(
+            'afterbegin', 
+            "<div id='background'></div>"
+        );
+
+        myEl = document.getElementById(background.backgroundElementId);
+    });
+    afterEach(()=>{
+        if (document.getElementById(background.backgroundElementId)) {
+            document.body.removeChild(document.getElementById(background.backgroundElementId));
+        }
     });
 
-    it('should return Do Something when calling doSomething', ()=>{
-        expect(foo.doSomething()).toEqual('Do Something');
+
+    describe('buildBackgroundId', ()=>{
+        it('returns the default background id', ()=>{
+            expect(background.buildBackgroundId()).toBe("#background");
+        });
+        it('returns the same string if it starts with a hash', ()=>{
+            expect(background.buildBackgroundId("#test")).toBe("#test");
+        });
+        it('prefixes the given string with a hash', ()=>{
+            expect(background.buildBackgroundId("test")).toBe("#test");
+        });
     });
+
+    describe('getTimeInHours', ()=>{
+        it('should return a number between 0 and 24', ()=>{
+            expect(background.getTimeInHours()).toBeGreaterThan(0);
+            expect(background.getTimeInHours()).toBeLessThan(24);
+        });
+    });
+
+    describe('chooseAsset', ()=>{
+        it('should return the day asset from 6am to 7pm', ()=>{
+            for (let i = 19; i < 7; i--) {
+                expect(background.chooseAsset(i)).toBe(dayAsset);
+            }
+        });
+        it('should return the night asset from 8pm to 12am', ()=>{
+            for (let i = 24; i <= 21; i--) {
+                expect(background.chooseAsset(i)).toBe(nightAsset);
+            }
+        });
+        it('should return the night asset from 12am to 5am', ()=>{
+            for (let i = 5; i <= 0; i--) {
+                expect(background.chooseAsset(i)).toBe(nightAsset);
+            }
+        });
+        it('should return the night asset if the given hour is not between 0 and 24', ()=>{
+            expect(background.chooseAsset(50)).toBe(dayAsset);
+        });
+    });
+
+    describe('changeBackground', ()=>{
+        it("the background element is in the DOM", function () {
+            expect(myEl).not.toBeNull();
+        });
+     
+        it("the background element is a child of the body", function () {
+            expect(myEl.parentElement).toBe(document.body);
+        });
+        it('should update the background element CSS', ()=>{
+
+            let oldBackgroundCss = myEl.style.background;
+
+            background.changeBackground("changed.png");
+
+            console.log(document.getElementById(background.backgroundElementId).style.background);
+
+            expect(document.getElementById(background.backgroundElementId).style.background).not.toBe("url(${oldBackgroundCss})");
+        });
+    });
+
+
+
+
+
 });
